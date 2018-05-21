53cf2d7b3f3b419cb88bc843988a3a9cfc829215
Tennyson Horn <thorn67@gmail.com>
2018-05-21 14:07:26 -0500

    Roughed out Mongoose Schema for Decks of Cards.

---
+++ models/deck.js
@@ -0,0 +1,40 @@
+'use-strict';
+
+const mongoose = require('mongoose');
+
+//Define the shape of a Card, to be nested into the Deck
+const CardSchema = new mongoose.Schema({
+    cardNumber: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    cardFront: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    cardBack: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+//Define the shape of the decks
+const DeckSchema = new mongoose.Schema({
+    deckName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    deckCards: [CardSchema],
+    deckAuthor: ObjectId,
+    createdAt: {type: Date, default: Date.now}
+});
+
+//instantiate the DeckSchema into a model
+const Deck = mongoose.model('Deck', DeckSchema);
+
+//make it available elsewhere
+module.exports = Deck;
